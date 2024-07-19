d00e3388242398a561deb1e3fcd34b91414c8149
Samilly <samillynunes19@gmail.com>
2024-07-19 13:10:58 -0300

    feat: view model added and fetching all companies

---
+++ lib/view_models/assets_view_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../data/models/models.dart';
+import '../data/repositories/assets_repository.dart';
+
+class AssetsViewModel extends ChangeNotifier {
+  final AssetsRepository assetsRepository;
+
+  AssetsViewModel({required this.assetsRepository});
+
+  List<CompanyModel> companies = [];
+  bool isLoading = false;
+  String? errorMsg;
+
+  Future fetchCompanies() async {
+    isLoading = true;
+    errorMsg = '';
+
+    try {
+      companies = await assetsRepository.getAllCompanies();
+    } catch (e) {
+      errorMsg = 'Failed to get companies';
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
