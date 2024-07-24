96d091538cd61135c1540bdd34b56fd1f0ad7e5e
Samilly <samillynunes19@gmail.com>
2024-07-24 18:46:13 -0300

    feat: reseting actions when going back to home

--- lib/views/assets_view.dart
+++ lib/views/assets_view.dart
@@ -33,97 +33,104 @@ class _AssetsViewState extends State<AssetsView> {
   Widget build(BuildContext context) {
     final viewModel = Provider.of<AssetsViewModel>(context);
 
-    return Scaffold(
-      appBar: const CustomAppbar(
-        label: 'Assets',
-        hasBackButton: true,
-      ),
-      body: Builder(
-        builder: (context) {
-          if (viewModel.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (viewModel.errorMsg?.isNotEmpty ?? false) {
-            return Center(
-              child: Text('${viewModel.errorMsg}'),
+    return PopScope(
+      onPopInvoked: (v) {
+        viewModel.setSensorFilterStatus(false);
+        viewModel.setCriticalSensorStatus(false);
+        viewModel.changeSearchingText('');
+      },
+      child: Scaffold(
+        appBar: const CustomAppbar(
+          label: 'Assets',
+          hasBackButton: true,
+        ),
+        body: Builder(
+          builder: (context) {
+            if (viewModel.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (viewModel.errorMsg?.isNotEmpty ?? false) {
+              return Center(
+                child: Text('${viewModel.errorMsg}'),
+              );
+            }
+
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SearchInput(
+                        onChange: viewModel.changeSearchingText,
+                        value: viewModel.searchingText,
+                        onSubmit: viewModel.submitSearchedText,
+                      ),
+                      Row(
+                        children: [
+                          FilterButton(
+                            label: 'Sensor de Energia',
+                            icon: Icons.bolt,
+                            isPressed: viewModel.sensorFilterIsPressed,
+                            onPressed: () {
+                              final currentStatus =
+                                  viewModel.sensorFilterIsPressed;
+                              viewModel.setSensorFilterStatus(!currentStatus);
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          FilterButton(
+                            label: 'Crítico',
+                            icon: Icons.warning_amber_rounded,
+                            isPressed: viewModel.criticalFilterIsPressed,
+                            onPressed: () {
+                              final currentStatus =
+                                  viewModel.criticalFilterIsPressed;
+                              viewModel.setCriticalSensorStatus(!currentStatus);
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(color: Colors.grey.shade300),
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: viewModel.locations.length +
+                        viewModel.unlinkedAssets.length,
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 10);
+                    },
+                    itemBuilder: (context, index) {
+                      final locationsLenght = viewModel.locations.length;
+
+                      LocationModel? location;
+                      AssetModel? asset;
+
+                      if (index >= locationsLenght) {
+                        final unlinkedIndex = index - locationsLenght;
+                        asset = viewModel.unlinkedAssets[unlinkedIndex];
+                      } else {
+                        location = viewModel.locations[index];
+                      }
+
+                      return AssetItem(
+                        iconUrl: location != null
+                            ? AppIcons.locationIcon
+                            : AppIcons.componentIcon,
+                        location: location,
+                        asset: asset,
+                      );
+                    },
+                  ),
+                ),
+              ],
             );
-          }
-
-          return Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SearchInput(
-                      onChange: viewModel.changeSearchingText,
-                      value: viewModel.searchingText,
-                      onSubmit: viewModel.submitSearchedText,
-                    ),
-                    Row(
-                      children: [
-                        FilterButton(
-                          label: 'Sensor de Energia',
-                          icon: Icons.bolt,
-                          isPressed: viewModel.sensorFilterIsPressed,
-                          onPressed: () {
-                            final currentStatus =
-                                viewModel.sensorFilterIsPressed;
-                            viewModel.setSensorFilterStatus(!currentStatus);
-                          },
-                        ),
-                        const SizedBox(width: 10),
-                        FilterButton(
-                          label: 'Crítico',
-                          icon: Icons.warning_amber_rounded,
-                          isPressed: viewModel.criticalFilterIsPressed,
-                          onPressed: () {
-                            final currentStatus =
-                                viewModel.criticalFilterIsPressed;
-                            viewModel.setCriticalSensorStatus(!currentStatus);
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Divider(color: Colors.grey.shade300),
-              Expanded(
-                child: ListView.separated(
-                  itemCount: viewModel.locations.length +
-                      viewModel.unlinkedAssets.length,
-                  separatorBuilder: (context, index) {
-                    return const SizedBox(height: 10);
-                  },
-                  itemBuilder: (context, index) {
-                    final locationsLenght = viewModel.locations.length;
-
-                    LocationModel? location;
-                    AssetModel? asset;
-
-                    if (index >= locationsLenght) {
-                      final unlinkedIndex = index - locationsLenght;
-                      asset = viewModel.unlinkedAssets[unlinkedIndex];
-                    } else {
-                      location = viewModel.locations[index];
-                    }
-
-                    return AssetItem(
-                      iconUrl: location != null
-                          ? AppIcons.locationIcon
-                          : AppIcons.componentIcon,
-                      location: location,
-                      asset: asset,
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
-        },
+          },
+        ),
       ),
     );
   }
