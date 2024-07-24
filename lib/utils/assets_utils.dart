0929268099b826c6808cb03e08e745a73503e1f1
Samilly <samillynunes19@gmail.com>
2024-07-24 11:40:24 -0300

    clean: moved functions to assets utils

--- lib/utils/assets_utils.dart
+++ lib/utils/assets_utils.dart
@@ -23,4 +23,83 @@ class AssetsUtils {
 
     return allAssets;
   }
+
+  static List<LocationModel> formatLocationListWithSublocations(
+      List<LocationModel> locations) {
+    final rootLocations = <LocationModel>[];
+    final waitingSublocations = <LocationModel>[];
+
+    for (var local in locations) {
+      if (local.isSubLocation()) {
+        final rootList =
+            rootLocations.where((l) => l.id == local.parentId).toList();
+
+        if (rootList.isNotEmpty) {
+          rootList[0].subLocations.add(local);
+        } else {
+          waitingSublocations.add(local);
+        }
+      } else {
+        rootLocations.add(local);
+      }
+    }
+
+    for (var sublocation in waitingSublocations) {
+      final rootList =
+          rootLocations.where((l) => l.id == sublocation.parentId).toList();
+
+      if (rootList.isNotEmpty) {
+        rootList[0].subLocations.add(sublocation);
+      }
+    }
+
+    return rootLocations;
+  }
+
+  static Map<String, List<AssetModel>> formatAssetsListWithSubassets(
+      List<AssetModel> assets) {
+    final assetsTemp = assets;
+    final unlinked = <AssetModel>[];
+    for (var asset in assetsTemp) {
+      if (asset.isUnliked()) {
+        unlinked.add(asset);
+        continue;
+      }
+
+      final subAssets =
+          assetsTemp.where((a) => a.parentId == asset.id).toList();
+      asset.childAssets = subAssets;
+
+      // chamada recursiva para pegar as pecas filhas das outras subpecas
+      if (subAssets.isNotEmpty) {
+        AssetsUtils.formatAssetsListWithSubassets(subAssets);
+      }
+    }
+
+    final map = {'unlinked': unlinked, 'assets': assetsTemp};
+
+    return map;
+  }
+
+  static List<LocationModel> joinAssetsWithLocations(
+      List<LocationModel> allLocals, List<AssetModel> allAssets) {
+    final locals = allLocals;
+    for (var local in locals) {
+      // check assets for the sublocation
+      for (var sublocation in local.subLocations) {
+        final sublocationAssets =
+            allAssets.where((a) => a.locationId == sublocation.id).toList();
+
+        sublocation.assets = sublocationAssets;
+      }
+
+      // check this location assets
+      final assetsFromLocal =
+          allAssets.where((asset) => asset.locationId == local.id).toList();
+
+      local.assets = assetsFromLocal;
+    }
+
+    return locals;
+  }
 }
--- lib/view_models/assets_view_model.dart
+++ lib/view_models/assets_view_model.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:tractian_assets_mobile/data/types/types.dart';
-import 'package:tractian_assets_mobile/utils/assets_utils.dart';
 
 import '../data/models/models.dart';
 import '../data/repositories/assets_repository.dart';
+import '../data/types/types.dart';
+import '../utils/assets_utils.dart';
 
 class AssetsViewModel extends ChangeNotifier {
   final AssetsRepository assetsRepository;
@@ -61,34 +61,10 @@ class AssetsViewModel extends ChangeNotifier {
         final allLocationsList =
             await assetsRepository.getAllLocations(companySelected!.id);
 
-        final rootLocations = <LocationModel>[];
-        final waitingSublocations = <LocationModel>[];
+        final formatedLocations =
+            AssetsUtils.formatLocationListWithSublocations(allLocationsList);
 
-        for (var local in allLocationsList) {
-          if (local.isSubLocation()) {
-            final rootList =
-                rootLocations.where((l) => l.id == local.parentId).toList();
-
-            if (rootList.isNotEmpty) {
-              rootList[0].subLocations.add(local);
-            } else {
-              waitingSublocations.add(local);
-            }
-          } else {
-            rootLocations.add(local);
-          }
-        }
-
-        for (var sublocation in waitingSublocations) {
-          final rootList =
-              rootLocations.where((l) => l.id == sublocation.parentId).toList();
-
-          if (rootList.isNotEmpty) {
-            rootList[0].subLocations.add(sublocation);
-          }
-        }
-
-        return rootLocations;
+        return formatedLocations;
       } else {
         errorMsg = 'Select a company';
         return [];
@@ -102,52 +78,6 @@ class AssetsViewModel extends ChangeNotifier {
     }
   }
 
-  List<AssetModel> _getSubAssets(List<AssetModel> allAssets) {
-    final assetsTemp = allAssets;
-    final unlinked = <AssetModel>[];
-    for (var asset in assetsTemp) {
-      if (asset.isUnliked()) {
-        unlinked.add(asset);
-        continue;
-      }
-
-      final subAssets =
-          assetsTemp.where((a) => a.parentId == asset.id).toList();
-      asset.childAssets = subAssets;
-
-      // chamada recursiva para pegar as pecas filhas das outras subpecas
-      if (subAssets.isNotEmpty) {
-        _getSubAssets(subAssets);
-      }
-    }
-
-    unlinkedAssets = unlinked;
-
-    return assetsTemp;
-  }
-
-  List<LocationModel> _getAssetsFromLocal(
-      List<LocationModel> allLocals, List<AssetModel> allAssets) {
-    final locals = allLocals;
-    for (var local in locals) {
-      // check assets for the sublocation
-      for (var sublocation in local.subLocations) {
-        final sublocationAssets =
-            allAssets.where((a) => a.locationId == sublocation.id).toList();
-
-        sublocation.assets = sublocationAssets;
-      }
-
-      // check this location assets
-      final assetsFromLocal =
-          allAssets.where((asset) => asset.locationId == local.id).toList();
-
-      local.assets = assetsFromLocal;
-    }
-
-    return locals;
-  }
-
   Future fetchAssets() async {
     isLoading = true;
     errorMsg = '';
@@ -159,13 +89,22 @@ class AssetsViewModel extends ChangeNotifier {
         final allAssets =
             await assetsRepository.getAllAssets(companySelected!.id);
 
-        final formatedAssets = _getSubAssets(allAssets);
+        final formatedAssetsMap =
+            AssetsUtils.formatAssetsListWithSubassets(allAssets);
 
-        final sortedAssets = AssetsUtils.sortAssetsList(formatedAssets);
+        if (formatedAssetsMap.containsKey('unlinked')) {
+          unlinkedAssets = formatedAssetsMap['unlinked']!;
+        }
+
+        final sortedAssets = AssetsUtils.sortAssetsList(
+            formatedAssetsMap.containsKey('assets')
+                ? formatedAssetsMap['assets']!
+                : []);
 
         assets = sortedAssets;
 
-        final formatedLocations = _getAssetsFromLocal(localsCopy, sortedAssets);
+        final formatedLocations =
+            AssetsUtils.joinAssetsWithLocations(localsCopy, sortedAssets);
 
         final sortedLocations = AssetsUtils.sortLocationList(formatedLocations);
 
@@ -200,14 +139,22 @@ class AssetsViewModel extends ChangeNotifier {
         return condition1 && condition2;
       }).toList();
 
-      final filteredSubassets = _getSubAssets(filteredAssets);
+      final assetsMap =
+          AssetsUtils.formatAssetsListWithSubassets(filteredAssets);
+
+      unlinkedAssets =
+          assetsMap.containsKey('unlinked') ? assetsMap['unlinked']! : [];
+
+      final filteredSubassets = assetsMap.containsKey('assets')
+          ? assetsMap['assets']!
+          : <AssetModel>[];
 
       locations = [];
 
       final localsCopy = await _fetchLocations();
 
       final formatedLocations =
-          _getAssetsFromLocal(localsCopy, filteredSubassets);
+          AssetsUtils.joinAssetsWithLocations(localsCopy, filteredSubassets);
 
       final sortedLocations = AssetsUtils.sortLocationList(formatedLocations);
 
